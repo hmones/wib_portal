3ba6fd54c8eef03ba6e104e56eeee73c8814b3f1
haythameyd@gmail.com <Enas30002909>
2020-01-29 02:29:08 +0100

    Added favicons

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -11,3 +11,8 @@
     integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
     crossorigin="anonymous"></script>
 <script type="application/javascript" src="{{asset('dist/semantic.min.js')}}"></script>
+
+<link rel="apple-touch-icon" sizes="180x180" href="https://gpp-wib-staging.frb.io/assets/img/favicons/wib/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="https://gpp-wib-staging.frb.io/assets/img/favicons/wib/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="https://gpp-wib-staging.frb.io/assets/img/favicons/wib/favicon-16x16.png">
+<link rel="manifest" href="https://gpp-wib-staging.frb.io/assets/img/favicons/wib/site.webmanifest">
