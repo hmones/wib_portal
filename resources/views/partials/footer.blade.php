306b5c4c229e6aab5cbc0d074632aaf97f773bff
haythameyd@gmail.com <Enas30002909>
2020-01-31 03:12:10 +0100

    Fixed the footer to match with the original website

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,7 +1,12 @@
 <br><br><br>
-<div class="ui inverted vertical footer very padded segment">
+<div class="ui blue inverted vertical footer very padded segment">
     <div class="ui container">
         <div class="ui stackable inverted divided equal height stackable grid">
+            <div class="five wide column">
+                <div class="ui image">
+                    <img src="{{asset('images/logo_inverted.png')}}" alt="Women in Business">
+                </div>
+            </div>
             <div class="five wide column">
                 <h4 class="ui inverted header">Our Partners</h4>
                 <div class="ui inverted link list">
@@ -13,17 +18,17 @@
                     <a href="http://www.global-project-partners.de/" class="item">Global Project Partners</a>
                 </div>
             </div>
-            <div class="five wide column">
+            <div class="six wide column">
                 <h4 class="ui inverted header">Other Links</h4>
                 <div class="ui inverted link list">
                     <a href="https://gpp-wib-staging.frb.io/impressium-example" class="item"> Imprint </a>
                     <a href="https://gpp-wib-staging.frb.io/data-privacy" class="item"> Data Privacy </a>
                 </div>
             </div>
-            <div class="six wide column">
-                <h4 class="ui inverted header">Imprint</h4>
-                <p>Copyrights reserved © 2019 Women in Business</p>
-            </div>
+        </div>
+        <br><br>
+        <div class="ui center aligned centered blue inverted padded basic segment">
+            <p>Copyrights reserved © 2019 Women in Business</p>
         </div>
     </div>
 </div>
