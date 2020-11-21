7c5f359906ad0ee0dceb732ec019bcb8cf726efa
hmones <haythameyd@gmail.com>
2020-11-21 15:13:29 +0100

    Adjusted the avatar partial to receive styles and classes

--- resources/views/partials/components/avatar.blade.php
+++ resources/views/partials/components/avatar.blade.php
@@ -1,21 +1,25 @@
+<img class="ui avatar {{isset($classes)?$classes:''}} image" src="
+
 @if($type == 'user')
 
 @if($user->image)
-<img class="ui avatar image" src="{{$user->image}}">
+{{$user->image}}
 @else
 @if ($user->gender === 'Female')
-<img class="ui avatar image" src="{{asset('images/female_avatar.jpg')}}">
+{{asset('images/female_avatar.jpg')}}
 @else
-<img class="ui avatar image" src="{{asset('images/male_avatar.jpg')}}">
+{{asset('images/male_avatar.jpg')}}
 @endif
 @endif
 
 @elseif($type == 'entity')
 
 @if($entity->image)
-<img class="ui avatar image" src="{{$entity->image}}">
+{{$entity->image}}
 @else
-<img class="ui avatar image" src="{{asset('images/logo_avatar.png')}}">
+{{asset('images/logo_avatar.png')}}
 @endif
 
 @endif
+
+" style="{{isset($styles)?$styles:''}}" />
