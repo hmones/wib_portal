e9e506a52548c441a09806c3c685ea0c0ea7e29b
hmones <haythameyd@gmail.com>
2021-02-19 22:12:26 +0100

    Fixed configuration and bash file

--- laradock_wib/elasticsearch/script/entry.sh
+++ laradock_wib/elasticsearch/script/entry.sh
@@ -2,5 +2,5 @@
 set -x
 sleep 30 &&
 docker exec laradock_wib_workspace_1 php artisan scout:import "App\Models\User"
-docker exec laradock_wib_workspace_1 scout:import "App\Models\Entity"
+docker exec laradock_wib_workspace_1 php artisan scout:import "App\Models\Entity"
 docker exec laradock_wib_workspace_1 php artisan scout:import "App\Models\Post"
