ba1e4766d547622065b329313e79a1dd69989d37
hmones <haythameyd@gmail.com>
2021-02-28 17:47:22 +0100

    Removed laradock from git tracking

--- laradock_wib/elasticsearch/script/entry.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -x
-sleep 30 &&
-docker exec laradock_wib_workspace_1 php artisan scout:import "App\Models\User"
-docker exec laradock_wib_workspace_1 php artisan scout:import "App\Models\Entity"
-docker exec laradock_wib_workspace_1 php artisan scout:import "App\Models\Post"
