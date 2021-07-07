6daad861a4b8396eaebb8aa6b85eb63435d9cd36
hmones <haythameyd@gmail.com>
2021-07-07 20:12:55 +0200

    Added basic tests and installed excel library

---
+++ app/Exports/EntitiesExport.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Exports;
+
+use Maatwebsite\Excel\Concerns\FromCollection;
+
+class EntitiesExport implements FromCollection
+{
+    public function collection()
+    {
+        //
+    }
+}
---
+++ app/Http/Resources/UserResource.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UserResource extends JsonResource
+{
+    public function toArray($request): array
+    {
+        return [
+            'title'                => $this->title,
+            'name'                 => $this->name,
+            'email'                => $this->email,
+            'image'                => $this->image,
+            'gender'               => $this->gender,
+            'birth_year'           => $this->birth_year,
+            'bio'                  => $this->bio,
+            'phone'                => '+' . $this->phone_country_code . $this->phone,
+            'country'              => optional($this->country)->name,
+            'city'                 => optional($this->city)->name,
+            'postal_code'          => $this->postal_code,
+            'education'            => $this->education,
+            'mena_diaspora'        => $this->mena_diaspora ? 'yes' : 'no',
+            'business_association' => $this->business_association_wom,
+            'profile_completion'   => $this->data_percent . '%',
+            'last_login'           => $this->last_login,
+            'created_at'           => $this->created_at
+        ];
+    }
+}
