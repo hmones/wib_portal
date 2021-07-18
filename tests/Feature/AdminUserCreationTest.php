b01aae392c1e08e76ba80d91b7d0eea47aee5aec
hmones <haythameyd@gmail.com>
2021-07-18 18:33:03 +0200

    Fixed sonar cube issues

--- tests/Feature/AdminUserCreationTest.php
+++ tests/Feature/AdminUserCreationTest.php
@@ -16,6 +16,7 @@ class AdminUserCreationTest extends TestCase
 {
     use DatabaseTransactions;
 
+    const UPDATE_ROUTE = 'admin.admins.update';
     protected $admin;
     protected $data = [
         'name'            => 'testAdmin',
@@ -42,28 +43,28 @@ class AdminUserCreationTest extends TestCase
 
     public function testAdminCanViewOtherAdmins(): void
     {
-        $admin = Admin::factory()->create();
+        $adminUser = Admin::factory()->create();
         $this->actingAs($this->admin, 'admin')
             ->post(route('admin.admins.index'))
-            ->assertSeeInOrder([$admin->name, $admin->email, $admin->created_at]);
+            ->assertSeeInOrder([$adminUser->name, $adminUser->email, $adminUser->created_at]);
     }
 
     public function testAdminCanDeleteOtherAdmins(): void
     {
-        $admin = Admin::factory()->create();
+        $adminUser = Admin::factory()->create();
 
-        Livewire::test(ShowAdmins::class)->call('destroy', $admin->id);
+        Livewire::test(ShowAdmins::class)->call('destroy', $adminUser->id);
 
-        $this->assertNull(Admin::find($admin->id));
-        $this->assertNull(User::where('approved_by', $admin->id)->first());
-        $this->assertNull(Entity::where('approved_by', $admin->id)->first());
+        $this->assertNull(Admin::find($adminUser->id));
+        $this->assertNull(User::where('approved_by', $adminUser->id)->first());
+        $this->assertNull(Entity::where('approved_by', $adminUser->id)->first());
     }
 
     public function testAdminCanUpdateTheirInformation(): void
     {
         Arr::forget($this->data, 'confirmPassword');
         $this->actingAs($this->admin, 'admin')
-            ->patch(route('admin.admins.update', $this->admin), array_merge($this->data, ['password_confirmation' => 'Password123']))
+            ->patch(route(self::UPDATE_ROUTE, $this->admin), array_merge($this->data, ['password_confirmation' => 'Password123']))
             ->assertRedirect(route('admin.home'))
             ->assertSessionHasNoErrors()
             ->assertSessionHas('success');
@@ -72,7 +73,7 @@ class AdminUserCreationTest extends TestCase
     public function testAdminCanUpdateTheirInformationEvenIfPasswordIsEmpty(): void
     {
         $this->actingAs($this->admin, 'admin')
-            ->put(route('admin.admins.update', $this->admin), [
+            ->put(route(self::UPDATE_ROUTE, $this->admin), [
                 'name'  => 'testAdmin',
                 'email' => 'test@test.test',
             ])->assertRedirect(route('admin.home'))
@@ -83,7 +84,7 @@ class AdminUserCreationTest extends TestCase
     public function testAdminCanNotUpdateTheirInformationWithInvalidData(): void
     {
         $this->actingAs($this->admin, 'admin')
-            ->put(route('admin.admins.update', $this->admin), array_merge($this->data, [
+            ->put(route(self::UPDATE_ROUTE, $this->admin), array_merge($this->data, [
                 'email'    => 'wrong_email.com',
                 'password' => 'differentPassword',
             ]))->assertSessionHasErrors(['email', 'password']);
@@ -91,9 +92,9 @@ class AdminUserCreationTest extends TestCase
 
     public function testAdminCanNotUpdateOtherAdminInformation(): void
     {
-        $admin = Admin::factory()->create();
+        $adminUser = Admin::factory()->create();
         $this->actingAs($this->admin, 'admin')
-            ->put(route('admin.admins.update', $admin), $this->data)
+            ->put(route(self::UPDATE_ROUTE, $adminUser), $this->data)
             ->assertStatus(Response::HTTP_FORBIDDEN);
     }
 
