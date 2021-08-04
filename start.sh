c7894ed2092247db453d9b7279be049d1b7a1b4e
hmones <haythameyd@gmail.com>
2021-08-04 20:45:30 +0200

    Installed sail for local development

--- start.sh
+++ start.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 set -x
 
-sudo sysctl vm.overcommit_memory=1
-sudo sysctl -w vm.max_map_count=262144
-cd laradock_wib
-docker-compose start && sleep 30 && docker-compose exec workspace php artisan scout:import "App\Models\User"
-docker-compose exec workspace php artisan scout:import "App\Models\Entity"
-docker-compose exec workspace php artisan scout:import "App\Models\Post"
+systemctl stop apache2.service
+systemctl stop mysql.service
+./vendor/bin/sail up -d
+
