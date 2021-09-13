a79d853c50b523d7a554a60c9f8b98e59d6de67c
Haytham Mones <haytham.mones@easybell.de>
2021-09-13 08:59:05 +0200

    Finalized homepage design

--- resources/views/partials/home/image-slide.blade.php
+++ resources/views/partials/home/image-slide.blade.php
@@ -2,9 +2,9 @@
     <div class="ui grey inverted basic segment">
         <div class="ui content basic segment">
             <p class="byline">NEXT EVENT</p>
-            <h1 class="ui blue header">{{$event->title}}</h1>
+            <h1 class="ui blue banner header">{{$event->title}}</h1>
             <p class="description">{{$event->description}}</p>
-            <a href="{{$event->link}}" class="ui link orange big button">
+            <a href="{{$event->link}}" class="ui orange link button">
                 <span class="ui blue text">{{$event->button_text}}</span>
             </a>
         </div>
@@ -33,7 +33,7 @@
     <div class="twelve wide centered center aligned column">
         <div class="ui center aligned basic segment">
             <p class="byline">NEXT EVENT</p>
-            <h1 class="ui blue header">{{$event->title}}</h1>
+            <h1 class="ui blue banner header">{{$event->title}}</h1>
         </div>
         <div class="event details"
              style="background: linear-gradient(180deg, rgba(0, 0, 0, 0) 61.95%, rgba(0, 0, 0, 0.8) 100%), url('{{$event->image}}');">
@@ -49,7 +49,7 @@
             </div>
         </div>
         <p class="description">{{$event->description}}</p>
-        <a href="{{$event->link}}" class="ui link orange big button">
+        <a href="{{$event->link}}" class="ui orange link button">
             <span class="ui blue text">{{$event->button_text}}</span>
         </a>
         <br><br>
